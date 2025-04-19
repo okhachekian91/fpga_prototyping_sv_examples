02131f813f97aee7553f804ec432dda56dc532fe
okhachekian91 <okhachekian@outlook.com>
2025-04-18 20:38:07 -0700

    added top level control fsm

--- 6.5.3_fibonacci_1/hdl/fibonacci_top.sv
+++ 6.5.3_fibonacci_1/hdl/fibonacci_top.sv
@@ -14,8 +14,10 @@ module fibonacci_top
 
     logic           start_db;
     logic [7:0]     bcd_i_next, bcd_i_reg;
-    logic           start, start_bcd2bin_reg; 
+    logic           start_bcd2bin;
     
+    logic [6:0]     bcd2bin; 
+    logic [4:0]     bin_i_next, bin_i_reg;
 
     enum logic [2:0] {  IDLE = 3'b000
 
@@ -25,13 +27,45 @@ module fibonacci_top
     always_comb
     begin
         ctl_next        = ctl_state;
+        bcd_i_next      = bcd_i_reg;
+        start_bcd2bin   = 1'b0;
+        bin_i_next      = bin_i_reg; 
+        start_fib       = 1'b0;
         case(ctl_state)
             IDLE:
             begin
                 if (start_db)
                 begin
                     bcd_i_next    = sw;
-                    start_bcd2bin = 1'b1; 
+                    op_next       = BCD2BIN; 
+                end
+            end
+            BCD2BIN:
+            begin
+                start_bcd2bin = 1'b1;
+                op_next       = FIB_GEN;
+            end
+            WAIT_BCD2BIN:
+            begin
+                if (done_bcd2bin)
+                begin
+                    if (bcd2bin > 'd31)
+                        bin_i_next = 'd31; 
+                    else
+                        bin_i_next = bcd2bin[4:0];
+                    
+                    op_next = FIB_GEN; 
+                end
+            end
+            FIB_GEN:
+            begin
+                start_fib = 1'b1;
+                op_next   = WAIT_FIB_GEN; 
+            end
+            WAIT_FIB_GEN:
+            begin
+                if (done_fib)
+                begin
                 end
             end
         endcase
@@ -66,8 +100,8 @@ module fibonacci_top
         .rst_n              (rst_n),
         .start              (start_bcd2bin),
         .done               (done_bcd2bin),
-        .bcd_in             (i_bcd),
-        .bin_out            (bcd2bin_out)
+        .bcd_in             (bcd_i_reg),
+        .bin_out            (bcd2bin)
     );
     
     // fibonacci generator
@@ -78,7 +112,7 @@ module fibonacci_top
         .ready              (ready),
         .start              (start_fib),
         .done_tick          (done_fib),
-        .i                  (),
+        .i                  (bin_i_reg),
         .fibo_num           (fibo_num),
         .ovf                (ovf)
     );
