6882010b7d40bc6e77d1a45346d9b038491e6382
okhachekian91 <okhachekian@outlook.com>
2025-06-06 18:57:49 -0700

    added section 6.5.5 auto scaled frequency counter

---
+++ 6.5.5_auto_scaled_frequency_counter/hdl/bin2bcd_ext.sv
@@ -0,0 +1,110 @@
+module bin2bcd_ext
+(
+    input  logic            clk,
+    input  logic            rst_n,
+
+    input  logic            start,
+    input  logic [13:0]     bin,
+    output logic            ready,
+    output logic            done_tick,
+    output logic [3:0]      bcd3,
+    output logic [3:0]      bcd2,
+    output logic [3:0]      bcd1,
+    output logic [3:0]      bcd0
+);
+
+
+enum logic [1:0] {  IDLE = 2'b00,
+                    OP   = 2'b01,
+                    DONE = 2'b10
+} op_state, op_next; 
+
+logic [13:0] p2s_reg, p2s_next; 
+logic [3:0]  n_reg, n_next; 
+logic [3:0]  bcd3_reg, bcd2_reg, bcd1_reg, bcd0_reg; 
+logic [3:0]  bcd3_next, bcd2_next, bcd1_next, bcd0_next; 
+logic [3:0]  bcd3_temp, bcd2_temp, bcd1_temp, bcd0_temp; 
+
+always_ff @(posedge clk)
+begin
+    if (!rst_n)
+    begin
+        op_state        <= IDLE; 
+        p2s_reg         <= 'b0;
+        n_reg           <= 'b0;
+        bcd3_reg        <= 'b0;
+        bcd2_reg        <= 'b0;
+        bcd1_reg        <= 'b0;
+        bcd0_reg        <= 'b0;
+    end
+    else
+    begin
+        op_state        <= op_next;
+        p2s_reg         <= p2s_next;
+        n_reg           <= n_next;
+        bcd3_reg        <= bcd3_next;
+        bcd2_reg        <= bcd2_next;
+        bcd1_reg        <= bcd1_next;
+        bcd0_reg        <= bcd0_next;
+    end
+end
+
+always_comb
+begin
+    op_next     = op_state;
+    ready       = 1'b0;
+    done_tick   = 1'b0;
+    p2s_next    = 'b0;
+    bcd0_next   = bcd0_reg;
+    bcd1_next   = bcd1_reg;
+    bcd2_next   = bcd2_reg;
+    bcd3_next   = bcd3_reg;
+    n_next      = n_reg;
+    case(op_state)
+        IDLE:
+        begin
+            ready = 1'b1;
+
+            if (start)
+            begin
+                op_next     = OP;
+                bcd3_next   = 'b0;
+                bcd2_next   = 'b0;
+                bcd1_next   = 'b0;
+                bcd0_next   = 'b0;
+                n_next      = 4'b1110;
+                p2s_next    = bin;
+            end
+        end
+        OP:
+        begin
+            p2s_next    = p2s_reg << 1; 
+
+            bcd0_next   = {bcd0_temp[2:0], p2s_reg[13]};
+            bcd1_next   = {bcd1_temp[2:0], bcd0_temp[3]};
+            bcd2_next   = {bcd2_temp[2:0], bcd1_temp[3]};
+            bcd3_next   = {bcd3_temp[2:0], bcd2_temp[3]};
+            n_next      = n_reg - 1; 
+            if (n_next == 0)
+                op_next     = DONE; 
+        end
+        DONE:
+        begin
+            done_tick   = 1'b1;
+            op_next     = IDLE;
+        end
+        default: op_next = IDLE; 
+    endcase
+end
+
+assign bcd0_temp = (bcd0_reg > 4) ? bcd0_reg + 3 : bcd0_reg;
+assign bcd1_temp = (bcd1_reg > 4) ? bcd1_reg + 3 : bcd1_reg; 
+assign bcd2_temp = (bcd2_reg > 4) ? bcd2_reg + 3 : bcd2_reg;
+assign bcd3_temp = (bcd3_reg > 4) ? bcd3_reg + 3 : bcd3_reg;
+
+assign bcd0 = bcd0_reg;
+assign bcd1 = bcd1_reg;
+assign bcd2 = bcd2_reg;
+assign bcd3 = bcd3_reg;
+
+endmodule
---
+++ 6.5.5_auto_scaled_frequency_counter/hdl/period_counter.sv
@@ -0,0 +1,98 @@
+module period_counter
+(
+   input  logic 	clk,
+   input  logic 	rst_n,
+
+   input  logic      	start,
+   input  logic 	sig,
+
+   output logic 	ready,
+   output logic 	done_tick,
+
+   output logic [19:0]  period
+);
+
+   localparam CLK_US_COUNT = 100;
+
+   enum logic [1:0] { IDLE  = 2'b00,
+                      WAIT  = 2'b01,
+                      COUNT = 2'b10,
+                      DONE  = 2'b11
+                    } state_reg, state_next;
+
+   logic                       	    edge_tick; 
+   logic 			    sig_dly; 
+   logic [$clog2(CLK_US_COUNT)-1:0] t_next,t_reg;
+   logic [19:0]                     p_next,p_reg;
+
+   always_ff @(posedge clk)
+   begin
+      if (!rst_n)
+         sig_dly <= 1'b0;
+      else
+         sig_dly <= sig;
+    end
+
+    assign edge_tick = sig && (!sig_dly); 
+    
+    always_comb
+    begin
+       state_next = state_reg;
+       ready      = 1'b0; 
+       done_tick  = 1'b0;
+       t_next     = t_reg;
+       p_next     = p_reg;
+       case(state_reg)
+          IDLE:
+          begin
+             ready = 1'b1; 
+             if (start)
+             begin
+                p_next     = 'b0;
+                t_next     = 'b0;
+                state_next = WAIT;
+             end
+          end
+          WAIT:
+          begin
+             if (edge_tick)
+                state_next = COUNT;
+          end
+          COUNT:
+          begin
+             t_next = t_reg + 1; 
+             if (edge_tick)
+                state_next = DONE;
+             else if (t_reg == CLK_US_COUNT-1)
+             begin
+                t_next = 'b0;
+                p_next = p_reg + 1;
+             end 
+          end
+          DONE:
+          begin
+             done_tick = 1'b1;
+             state_next = IDLE;
+          end
+       endcase
+    end
+
+    always_ff @(posedge clk)
+    begin
+       if (!rst_n)
+       begin
+          state_reg <= IDLE;
+          t_reg     <= 'b0;
+          p_reg     <= 'b0;
+       end
+       else
+       begin
+          state_reg <= state_next;
+          t_reg     <= t_next;
+          p_reg     <= p_next;
+       end
+    end
+
+    assign period = p_reg;
+
+endmodule
