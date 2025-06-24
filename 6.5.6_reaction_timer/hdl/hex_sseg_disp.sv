eeb6c8c21663d56613a3e25cb8ee204eef0c2367
okhachekian91 <okhachekian@outlook.com>
2025-06-23 19:57:49 -0700

    added reaction timer

---
+++ 6.5.6_reaction_timer/hdl/hex_sseg_disp.sv
@@ -0,0 +1,81 @@
+module hex_sseg_disp
+(
+    input  logic        clk,
+    input  logic        rst_n,
+
+    input  logic [3:0]  bcd0,
+    input  logic [3:0]  bcd1,
+    input  logic [3:0]  bcd2, 
+    input  logic [3:0]  bcd3,
+
+    output logic [7:0]  sseg,
+    output logic [7:0]  an
+
+);
+
+    logic [16:0] counter; 
+    logic [3:0]  hex; 
+    logic [3:0]  dp_decode;
+    logic        dp_bit;
+
+    always_ff @(posedge clk)
+    begin
+        if (!rst_n)
+            counter <= 'b0;
+        else
+            counter <= counter + 1;
+    end
+
+    always_comb
+    begin
+        case(counter[16:15])
+            2'b00:
+            begin
+                hex     = bcd0;
+                an      = 8'b11111110;
+                dp_bit  = !dp_decode[0];
+            end
+            2'b01:
+            begin
+                hex     = bcd1;
+                an      = 8'b11111101;
+                dp_bit  = !dp_decode[1];
+            end
+            2'b10:
+            begin
+                hex     = bcd2;
+                an      = 8'b11111011;
+                dp_bit  = !dp_decode[2];
+            end
+            2'b11:
+            begin
+                hex     = bcd3;
+                an      = 8'b11110111;
+                dp_bit  = !dp_decode[3];
+            end
+        endcase
+    end
+
+    always_comb
+    begin
+        case(hex)
+	   4'h0: sseg[6:0] = 7'b1000000;
+	   4'h1: sseg[6:0] = 7'b1111001;
+	   4'h2: sseg[6:0] = 7'b0100100;
+	   4'h3: sseg[6:0] = 7'b0110000;
+	   4'h4: sseg[6:0] = 7'b0011001;
+	   4'h5: sseg[6:0] = 7'b0010010;
+	   4'h6: sseg[6:0] = 7'b0000010;
+	   4'h7: sseg[6:0] = 7'b1111000;
+	   4'h8: sseg[6:0] = 7'b0000000;
+	   4'h9: sseg[6:0] = 7'b0010000;
+	   4'ha: sseg[6:0] = 7'b0001000;
+	   4'hb: sseg[6:0] = 7'b0000011;
+	   4'hc: sseg[6:0] = 7'b1000110;
+	   4'hd: sseg[6:0] = 7'b0100001;
+	   4'he: sseg[6:0] = 7'b0000110;
+	   4'hf: sseg[6:0] = 7'b0001110;
+	endcase
+	sseg[7] = 1'b1;
+   end
+endmodule
---
+++ 6.5.6_reaction_timer/hdl/reaction_timer.sv
@@ -0,0 +1,201 @@
+module reaction_timer
+(
+    input  logic        clk,
+    input  logic        rst_n,
+
+    input  logic        start,
+    input  logic        stop,
+    input  logic        clear,
+
+    output logic        stim_led,
+    output logic [7:0]  sseg,
+    output logic [7:0]  an
+);
+    localparam MS_COUNT  = 100000;
+    localparam SEC_COUNT = 100000000;
+
+    logic [16:0]    timer;
+    logic [13:0]    ms_count;
+    logic [3:0]     lfsr;
+
+    logic           start_db;
+
+    logic [26:0]    stim_timer;
+    logic [4:0]     sec_count;
+
+    logic           stim_next;
+	
+	logic 			start_db, stop_db, clr_db;
+	
+	logic [3:0] 	bcd0, bcd1, bcd2, bcd3;
+	
+    always_ff @(posedge clk)
+    begin
+        if (!rst_n)
+        begin
+            timer    <= 'b0;
+            ms_count <= 'b0;
+        end
+        else if (clr_db)
+        begin
+            timer    <= 'b0;
+            ms_count <= 'b0;
+        end
+        else if (state_reg == WAIT)
+        begin
+            if (timer == MS_COUNT-1)
+            begin
+                timer <= 'b0;
+                if (ms_count < 'd9999)
+                    ms_count <= ms_count + 1; 
+            end
+            else
+                timer <= timer + 1; 
+        end
+    end
+
+    always_ff @(posedge clk)
+    begin
+        if (!rst_n)
+            lfsr = 4'hF;
+        else if (state_reg == IDLE)
+            lfsr = {lfsr[2:0], lfsr[3] ^ lfsr[2]};
+    end
+
+    always_ff @(posedge clk)
+    begin
+        if (!rst_n)
+        begin
+            stim_timer <= 'b0;
+            sec_count  <= 'b0;
+        end
+        else if (state_reg == START)
+        begin
+            if (stim_timer == SEC_COUNT-1)
+            begin
+                stim_timer <= 'b0;
+                sec_count  <= sec_count + 1;
+            end
+            else
+                stim_timer <= stim_timer + 1;
+        end
+        else
+        begin
+            stim_timer <= 'b0;
+            sec_count  <= 'b0;
+        end
+    end
+
+    enum logic [2:0] { IDLE  = 3'b000,
+                       START = 3'b001,
+                       STIM  = 3'b010,
+                       WAIT  = 3'b011,
+                       DONE  = 3'b100
+   } state_reg, state_next;
+   
+   always_comb
+   begin
+        state_next = state_reg;
+        stim_next  = stim_led;
+        case(state_reg)
+            IDLE:
+            begin
+
+                if (start_db)
+                    state_next = START;
+            end
+            START:
+            begin
+                if ((sec_count == lfsr) && (sec_count >= 'd2))
+                    state_next = STIM;
+            end
+            STIM:
+            begin
+                stim_next  = 1'b1;
+                state_next = WAIT;
+            end
+            WAIT:
+            begin
+                if (stop_db)
+                begin
+                    stim_next = 1'b0;
+                    state_next = DONE;
+                end
+            end
+            DONE:
+            begin
+                if (clr_db)
+                    state_next = IDLE;
+            end
+            default:
+            begin
+                state_next = IDLE;
+                stim_led   = 1'b0;
+            end
+        endcase
+    end
+
+    always_ff @(posedge clk)
+    begin
+        if (!rst_n)
+        begin
+            state_reg <= IDLE;
+            stim_led  <= 1'b0;
+        end
+        else
+        begin
+            state_reg <= state_next;
+            stim_led  <= stim_next;
+        end
+    end
+
+   early_detection_debounce u_start_db
+   (
+		.clk			(clk),
+		.rst_n			(rst_n),
+		.sw				(start),
+		.db				(start_db)
+   );
+   
+   early_detection_debounce u_stop_db
+   (
+		.clk			(clk),
+		.rst_n			(rst_n),
+		.sw 			(stop),
+		.db				(stop_db)
+   );
+   
+   early_detection_debounce u_clr_db
+   (
+		.clk 			(clk),
+		.rst_n			(rst_n),
+		.sw				(clr),
+		.db				(clr_db)
+   );
+   
+    bin2bcd u_bin2bcd
+    (
+        .clk            (clk),
+        .rst_n          (rst_n),
+        .start          (start_b2b),
+        .bin            (ms_count),
+        .ready          (),
+        .done_tick      (),
+        .bcd3           (bcd3),
+        .bcd2           (bcd2),
+        .bcd1           (bcd1),
+        .bcd0           (bcd0)
+    );
+    
+	hex_sseg_disp u_hex_sseg_disp
+	(
+		.clk			(clk),
+		.rst_n 			(rst_n),
+		.bcd3			(bcd3),
+		.bcd2			(bcd2),
+		.bcd1			(bcd1),
+		.bcd0			(bcd0),
+		.sseg			(sseg),
+		.an				(an)
+	);
+endmodule
